c853b0230371a21033b0ca6bead48db046982242
Vadym-Kuliabko <vadilatele@gmail.com>
2022-04-14 15:21:50 +0200

    Update FriendListItem.jsx
    fixed

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,7 @@ import { Wrapper, Status, Avatar, Name } from '../FriendList.styled';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <Wrapper>
-      <Status isOnline={isOnline ? 'true' : 'false'}>{isOnline}</Status>
+      <Status>{isOnline}</Status>
       <Avatar src={avatar} alt={name} width="48" />
       <Name>{name}</Name>
     </Wrapper>
